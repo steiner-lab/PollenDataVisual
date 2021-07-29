9a2f63a513d1d7fff6078f93752bd262f6774d52
nityanaki <nityanaki@gmail.com>
2021-07-29 14:43:57 -0400

    Adding logic for animate

--- animate.js
+++ animate.js
@@ -1,9 +1,23 @@
 // testing out the animate function with the current 3 days
-slider.value = slider.min;
-slider.dataset.currmon = valToDate();
-updateMap();
-updateHTML();
+// slider.value = slider.min;
+// slider.dataset.currmon = valToDate();
+// updateMap();
+// updateHTML();
 
+// hides/shows play and pause buttons 
+$('#pause').hide();
+
+document.querySelector("#play").addEventListener("click", function() {
+    $('#play').hide();
+    $('#pause').show();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+    $('#pause').hide();
+    $('#play').show();
+});
+
+// animates the map
 function animate() {
     if (slider.value < slider.max) {
         slider.value++;
@@ -13,4 +27,7 @@ function animate() {
     updateMap();
 }
 
-setInterval(animate, 1000);
+// add function to detect change in play/pause button
+// maybe make the play and pause button change a boolean: if animate = true for ex
+
+// setInterval(animate, 1000);
