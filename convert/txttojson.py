9dba36f22c96550d09df10bba55a90f5a47be5e3
nityanaki <nityanaki@gmail.com>
2021-08-06 12:36:24 -0400

    More json files

--- convert/txttojson.py
+++ convert/txttojson.py
@@ -54,7 +54,7 @@ def convertyr(poll_type, year):
         # white, vvlightpurple, vlightpurple, lightpurple, purple, blue, green, yellow, orange, red
         # colors = ['#f2f0e9', '#e7d8ed', '#e1c0f0', '#dfb1f2', '#BF55EC', '#2b83ba', '#abdda4', '#fcf065', '#fdae61', '#d7191c']
         colors = ['#f2f0e9', '#faedff', '#f1cfff', '#e8b6fc', '#BF55EC', '#2b83ba', '#abdda4', '#fcf065', '#fdae61', '#d7191c']
-        levels = [0, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
+        levels = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
     elif poll_type == "dbf":
         # diff shades of green
@@ -62,25 +62,25 @@ def convertyr(poll_type, year):
         # levels = [0, 500, 1000, 2000, 5000, 10000, 20000]
         colors = ['#f2f0e9', '#dceede', '#b9debd', '#96ce9d', '#74bd7c', '#51ad5c', '#418a49', '#306837', '#204524', '#102212']
         # levels = [0, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000]
-        levels = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000] # I think previous scale works better - this scale doesn't show contours
+        levels = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
     
     elif poll_type == "enf":
         # diff shades of orange
         # colors = ['#f2f0e9', '#ffd6b6', '#ffad6d', '#ff8424', '#da6000', '#914000', '#482000']
         colors = ['#f2f0e9', '#fee2cc', '#ffc699', '#ffa966', '#ff8d33', '#ff7000', '#cc5a00', '#994300', '#662d00','#331600']
-        levels = [0, 1000, 2000, 5000, 10000, 20000, 50000]
+        levels = [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 
     elif poll_type == "gra":
         # diff shades of brown
         # colors = ['#f2f0e9', '#edd2c7', '#dca68f', '#cb7a57', '#a75633', '#6f3922', '#371c11']
         colors = ['#f2f0e9', '#f3e0d7', '#e7c1b0', '#dba289', '#cf8362', '#c3643b', '#9c502f', '#753c23', '#4e2817', '#27140b']
-        levels = [0, 100, 200, 500, 1000, 2000, 5000]
+        levels = [0, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
 
     elif poll_type == "rag":
         # diff shades of red
         # colors = ['#f2f0e9', '#f4c0c0', '#ea8282', '#df4343', '#bb1f1f', '#7c1414', '#3e0a0a']
         colors = ['#f2f0e9', '#f7d3d3', '#f0a7a7', '#e97b7b', '#e24f4f', '#db2323', '#af1c1c', '#831515', '#570e0e', '#2b0707']
-        levels = [0, 100, 200, 500, 1000, 2000, 5000]
+        levels = [0, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
 
     df = pd.read_csv("txt/" + poll_type + "/" + poll_type + "max_" + str(year) + ".txt",
                                    sep="\s+|;|:",
@@ -107,8 +107,8 @@ def convert(poll_type, st_yr, en_yr):
         convertyr(poll_type, year)
 
 
-st_yr = 1998
-en_yr = 2002
+st_yr = 2003
+en_yr = 2016
 
 # convert("all", st_yr, en_yr)
 convert("dbf", st_yr, en_yr)
--- script.js
+++ script.js
@@ -69,15 +69,15 @@ function updateMap() {
 function getScale() {
     var type = document.querySelector('.buttons').dataset.type;
     if (type == "all") {
-        return [0, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000];
+        return [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000];
     } else if (type == "dbf") {
-        return [0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000];
+        return [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000];
     } else if (type == "enf") {
-        return [0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000];
+        return [0, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000];
     } else if (type == "gra") {
-        return [0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000];
+        return [0, 2, 5, 10, 20, 50, 100, 200, 500, 1000];
     } else { // when the type is ragweed
-        return [0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000];
+        return [0, 2, 5, 10, 20, 50, 100, 200, 500, 1000];
     }
 }
 
